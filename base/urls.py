05fd6c14cae43eac2cad48169985e2968f894fb5
Yash Agrahari <yashagrahari456@gmail.com>
2023-04-19 00:11:08 +0530

    User Login and Logout View

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns= [
     path('',TaskList.as_view(),name='tasks'),
+    path('login/',CustomLoginView.as_view(),name='login'),
+    path('logout/',LogoutView.as_view(next_page='login'),name='logout'),
     path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-edit/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
