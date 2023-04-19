f2a81ebd1a151d2cfa361a2d6fcc3d14f2476f11
Yash Agrahari <yashagrahari456@gmail.com>
2023-04-20 00:16:33 +0530

    Revert "Set Login Required to access or modify data"
    This reverts commit c305c501a85b376bfec3c5e9c4497d83abc101a4.

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-#=%a2(ryr!*ri+q=b^t9r29u*crt#wwg#$)9iaf&Yashu-D07C'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
