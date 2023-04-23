f87bb458ff53cf7a4569220c0b7589358b5011e7
Yash Agrahari <yashagrahari456@gmail.com>
2023-04-23 13:28:57 +0530

    fix some errors

--- base/views.py
+++ base/views.py
@@ -46,7 +46,7 @@ class TaskList(LoginRequiredMixin,ListView):
 
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
-            context['tasks'] = context['tasks'].filter(title_startswith=search_input)
+            context['tasks'] = context['tasks'].filter(title__startswith=search_input)
         context['search_input'] = search_input 
         return context
 
