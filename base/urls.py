97ce522a72113c6d87fcc9413e519db5ee639e73
Yash Agrahari <yashagrahari456@gmail.com>
2023-04-23 13:37:16 +0530

    fix registration page errors

--- base/urls.py
+++ base/urls.py
@@ -6,7 +6,7 @@ urlpatterns= [
     path('',TaskList.as_view(),name='tasks'),
     path('login/',CustomLoginView.as_view(),name='login'),
     path('logout/',LogoutView.as_view(next_page='login'),name='logout'),
-    path('register/', RegisterPage.as_view,name='register'),
+    path('register/', RegisterPage.as_view(),name='register'),
     path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-edit/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
